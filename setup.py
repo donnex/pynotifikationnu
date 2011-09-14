a6c23563190e3b3d10facef227dcaa854abf7bc9
Daniel Johansson <donnex@donnex.net>
2011-09-14 17:26:13 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='NotifikationNu',
+    version='1.0',
+    description='API library for interacting with Notifikation.nu.',
+    url='https://github.com/donnex/pynotifikationnu',
+    py_modules=['notifikation_nu'],
+
+    author='Daniel Johansson',
+    author_email='donnex@donnex.net',
+    license='BSD',
+)
