65bda51da8db4327de8214cfab4c5879a103aec4
Daniel Johansson <donnex@donnex.net>
2011-09-14 17:10:49 +0200

    Added support for overriding category and event

--- notifikation_nu.py
+++ notifikation_nu.py
@@ -17,16 +17,19 @@ class NotifikationNu(object):
         if len(self.api_key) != 40:
             raise ValueError('API key must be 40 characters.')
 
-    def send_notification(self, notification_id, message):
+    def send_notification(self, notification_id, message, category='', event=''):
         """Send a notification to the notification_id with message as
-        notification content.
+        notification content. Category and event will override the
+        values set on notifikation.nu.
         """
-        self.api_url = 'http://notifikation.nu/api/%s/send_notification/' % self.api_key
+        self.api_url = 'http://notifikation.nu/api/%s/send_notification/' % (self.api_key,)
         self.post_data = {
             'id': notification_id,
             'message': message,
+            'category': category,
+            'event': event,
         }
-        
+
         params = urllib.urlencode(self.post_data)
         self.api_reply_content = urllib.urlopen(self.api_url, params).read()
         self.api_reply = json.loads(self.api_reply_content)
